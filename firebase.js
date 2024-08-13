0e4a1bdc33afa532f3e4ce2f69be62f9dcbdc8ed
Muhammad Raffay <muhammadraffay@trentu.ca>
2024-08-12 22:26:25 -0400

    functionality complete

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
- apiKey: "YOUR_API_KEY",
- authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
- projectId: "YOUR_PROJECT_ID",
- storageBucket: "YOUR_PROJECT_ID.appspot.com",
- messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
- appId: "YOUR_APP_ID"
- };
+    apiKey: "AIzaSyDCgim03S73RBVvOOL7GS8K_kJUuGU0c9Y",
+    authDomain: "inventory-management-app-3336c.firebaseapp.com",
+    projectId: "inventory-management-app-3336c",
+    storageBucket: "inventory-management-app-3336c.appspot.com",
+    messagingSenderId: "68960865240",
+    appId: "1:68960865240:web:bb567c12c32e8b303640ad"
+  };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-export { firestore };
+
+export {firestore}
